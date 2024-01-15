1d2902bb5fe48412b7f2611f1a0ca877e458394f
CalinCretu <calincretu1412@gmail.com>
2024-01-15 16:49:14 +0100

    ADD - axios_request_confirm

---
+++ src/pages/projects/Index.vue
@@ -0,0 +1,41 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      projects: [],
+      BASE_ULR: 'http://127.0.0.1:8000/api'
+    }
+  },
+  methods: {
+    fetchProjects() {
+      axios.get(`${this.BASE_ULR}/projects`)
+        .then((res) => {
+          this.projects = res.data.results
+          console.log(res)
+        })
+    }
+  },
+  created() {
+    this.fetchProjects()
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>
+      I nostri ultimi progetti
+    </h1>
+  </div>
+  <div class="container">
+    <div class="grid">
+      <div class="card project-card" v-for="project in projects" key="project.id">
+        <h3>{{ project.name }}</h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss"></style>
